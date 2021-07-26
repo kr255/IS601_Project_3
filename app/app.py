4701d23e24d35f738b38238ef102849cf38d6f65
kamran raza <kr255@njit.edu>
2021-07-25 20:03:06 -0400

    edit function works

--- app/app.py
+++ app/app.py
@@ -38,31 +38,31 @@ def index():
 @app.route('/view/<int:city_id>', methods=['GET'])
 def record_view(city_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM addresses WHERE Postal_Code=%s', city_id)
+    cursor.execute('SELECT * FROM addresses WHERE Address_id=%s', city_id)
     result = cursor.fetchall()
     return render_template('view.html', title='View Form', city=result[0])
 
 
-# @app.route('/edit/<int:city_id>', methods=['GET'])
-# def form_edit_get(city_id):
-#     cursor = mysql.get_db().cursor()
-#     cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
-#     result = cursor.fetchall()
-#     return render_template('edit.html', title='Edit Form', city=result[0])
-#
-#
-# @app.route('/edit/<int:city_id>', methods=['POST'])
-# def form_update_post(city_id):
-#     cursor = mysql.get_db().cursor()
-#     inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
-#                  request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-#                  request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), city_id)
-#     sql_update_query = """UPDATE tblCitiesImport t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry =
-#     %s, t.fldAbbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
-#     cursor.execute(sql_update_query, inputData)
-#     mysql.get_db().commit()
-#     return redirect("/", code=302)
-#
+@app.route('/edit/<int:city_id>', methods=['GET'])
+def form_edit_get(city_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM addresses WHERE Address_id=%s', int(city_id))
+    result = cursor.fetchall()
+    return render_template('edit.html', title='Edit Form', city=result[0])
+
+
+@app.route('/edit/<int:city_id>', methods=['POST'])
+def form_update_post(city_id):
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
+                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
+                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), city_id)
+    sql_update_query = """UPDATE tblCitiesImport t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry =
+    %s, t.fldAbbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
 # @app.route('/cities/new', methods=['GET'])
 # def form_insert_get():
 #     return render_template('new.html', title='New City Form')
