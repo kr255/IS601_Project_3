15f429d844f6ac1782464515fc369c9cc7f549bd
kamran raza <kr255@njit.edu>
2021-07-25 21:18:18 -0400

    added word file

--- app/app.py
+++ app/app.py
@@ -79,15 +79,15 @@ def form_insert_post():
     mysql.get_db().commit()
     return redirect("/", code=302)
 
-# @app.route('/delete/<int:city_id>', methods=['POST'])
-# def form_delete_post(city_id):
-#     cursor = mysql.get_db().cursor()
-#     sql_delete_query = """DELETE FROM tblCitiesImport WHERE id = %s """
-#     cursor.execute(sql_delete_query, city_id)
-#     mysql.get_db().commit()
-#     return redirect("/", code=302)
-#
-#
+@app.route('/delete/<int:city_id>', methods=['POST'])
+def form_delete_post(city_id):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM addresses WHERE Address_id = %s """
+    cursor.execute(sql_delete_query, city_id)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
 # @app.route('/api/v1/cities', methods=['GET'])
 # def api_browse() -> str:
 #     cursor = mysql.get_db().cursor()
