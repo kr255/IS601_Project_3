f9d62c70036e80734bb9a88a77568125d80e40b7
kamran raza <kr255@njit.edu>
2021-07-25 20:54:07 -0400

    update and insert function works

--- app/app.py
+++ app/app.py
@@ -56,29 +56,29 @@ def form_update_post(city_id):
     cursor = mysql.get_db().cursor()
     inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
                  request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), city_id)
-    sql_update_query = """UPDATE tblCitiesImport t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry =
-    %s, t.fldAbbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
+                 int(request.form.get('fldCapitalStatus')), city_id)
+    sql_update_query = """UPDATE addresses t SET t.First_Name = %s, t.City = %s, t.State = %s, t.Street =
+    %s, t.Last_Name = %s, t.Postal_Code = %s WHERE t.Address_id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
-# @app.route('/cities/new', methods=['GET'])
-# def form_insert_get():
-#     return render_template('new.html', title='New City Form')
-#
-#
-# @app.route('/cities/new', methods=['POST'])
-# def form_insert_post():
-#     cursor = mysql.get_db().cursor()
-#     inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
-#                  request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-#                  request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'))
-#     sql_insert_query = """INSERT INTO tblCitiesImport (fldName,fldLat,fldLong,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES (%s, %s,%s, %s,%s, %s,%s) """
-#     cursor.execute(sql_insert_query, inputData)
-#     mysql.get_db().commit()
-#     return redirect("/", code=302)
-#
+@app.route('/addresses/new', methods=['GET'])
+def form_insert_get():
+    return render_template('new.html', title='New Address Form')
+
+
+@app.route('/addresses/new', methods=['POST'])
+def form_insert_post():
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
+                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
+                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'))
+    sql_insert_query = """INSERT INTO addresses (First_Name,City,State,Street,Last_Name,Postal_Code,Address_id) VALUES (%s, %s,%s, %s,%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
 # @app.route('/delete/<int:city_id>', methods=['POST'])
 # def form_delete_post(city_id):
 #     cursor = mysql.get_db().cursor()
